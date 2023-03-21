41fc38aad197bd5af4598fa44d685f3c312e4c30
Aabhusan Aryal <aabhusanaryal@gmail.com>
2023-03-21 22:38:25 +0545

    Removed test code

--- main.py
+++ main.py
@@ -114,10 +114,6 @@ app.add_middleware(
     allow_headers=['*'],    
 )
 
-@app.get("/test")
-async def get_locations():
-    return "Testing new server (2)"
-
 @app.post("/token")
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),db:Session=Depends(get_db)):
     user = await authenticate_user(form_data.username, form_data.password,db)
